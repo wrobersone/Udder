7252f9bf2ebb1067b10df5c061a56883fb25d997
William Robersone <william.robersone@gmail.com>
2020-01-08 13:56:52 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by William Robersone on 1/8/20.
+//  Copyright © 2020 William Robersone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
